5a865f654ad8a9b75632364f414464e5b0e516da
Matthew <matt-hope@hotmail.com>
2020-11-21 16:35:21 -0500

    added snake component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Snake from './Snake';
 
 function App() {
   return (
     <div className="App game-area">
-
+      <Snake />
     </div>
   );
 }
---
+++ src/Snake.js
@@ -0,0 +1,19 @@
+import React , { Component } from 'react';
+
+class Snake extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                <h1>I am a snake</h1>
+            </div>
+        )
+    }
+}
+
+export default Snake;
